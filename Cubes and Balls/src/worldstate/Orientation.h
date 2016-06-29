60e81e8e404c237c789fe38727ce54c3efd20db1
Rubisk <woheri@gmail.com>
2016-06-30 01:20:39 +0200

    Added initializer to Orientation.

--- Cubes and Balls/src/worldstate/Orientation.h
+++ Cubes and Balls/src/worldstate/Orientation.h
@@ -7,8 +7,9 @@
 // frontSide is the way (0, 0, 1) is pointing, upSide is the way 
 // (0, 1, 0) is pointing. This represents a unique orientation.
 struct Orientation {
-	glm::vec3 frontSide;
-	glm::vec3 upSide;
+
+	glm::vec3 frontSide = glm::vec3(0, 0, 1);
+	glm::vec3 upSide = glm::vec3(0, 1, 0);
 
 	// Rotate an orientation along axis
 	void Rotate(const glm::vec3 &axis, float degrees);
