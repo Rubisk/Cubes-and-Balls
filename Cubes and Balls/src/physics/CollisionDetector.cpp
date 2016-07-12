591f7c0d4b02167107cf120e4a0b8d1aa1177dca
Rubisk <woheri@gmail.com>
2016-07-12 18:51:46 +0200

    Rewrote collisiondetector

--- Cubes and Balls/src/physics/CollisionDetector.cpp
+++ Cubes and Balls/src/physics/CollisionDetector.cpp
@@ -29,6 +29,16 @@ struct Face {
 	vec3 p3;
 };
 
+struct Line2D {
+	vec2 p1;
+	vec2 p2;
+};
+
+struct Line3D {
+	vec3 p1;
+	vec3 p2;
+};
+
 vec3 SwapAxi(const vec3 &target, int newZ) {
 	switch (newZ) {
 	case 0:
@@ -79,51 +89,151 @@ vec3 GetCollisionDirection(shared_ptr<const Model> model, const vector<bool> &fa
 		vec3 faceNormal = GetFaceNormalFromModel(model, i);
 		normalSum += faceNormal;
 	}
-	return -normalize(normalSum);
+	return (length(normalSum) > 0.000001f) ? -normalize(normalSum) : vec3(0, 0, 0);
 }
 
-// Use this to find the value of a model face at a 2d point.
-// Returns false if the face doesn't have any value there, and true otherwise.
-// If the face has a value at that point, it writes the value to the corresponding input float.
-// The other 2 floats are used to specify the point to look at.
-bool GetValueAtPoint(const vec3 &v1, const vec3 &v2, const vec3 &v3, float x, float y, float &outputZ) {
+// Returns the intersection point of two lines if it exists.
+// If lines are perpendicular, or don't intersect, returns false. Also returns false if lines overlap somewhere.
+// Returns true if they do intersect, and writes the intersection point to outputIntersection.
+bool GetIntersection(const Line2D &first, const Line2D &second, vec2 &outputIntersection) {
+	for (int i = 0; i < 3; i++) {
+		if (min(first.p1[i], first.p2[i]) > max(second.p1[i], second.p2[i])) return false;
+		if (min(second.p1[i], second.p2[i]) > max(first.p1[i], first.p2[i])) return false;
+	}
+
+	mat2 system = transpose(
+		mat2{first.p2.x - first.p1.x, second.p1.x - second.p2.x,
+		     first.p2.y - first.p1.y, second.p1.y - second.p2.y});
+	if (determinant(system) == 0) return false; // perpendicular lines
+
+	vec2 solution = inverse(system) * vec2(second.p1.x - first.p1.x, second.p1.y - first.p1.y);
+
+	if (solution[0] < 0 || solution[0] > 1 || solution[1] < 0 || solution[1] > 1) return false; // lines too short to intersect.
+	outputIntersection = (1 - solution[0]) * first.p1 + solution[0] * first.p2;
+	return true;
+};
+
+bool GetLineMinMaxAtPoint(const Line3D &line, vec2 point, float &outputMinZ, float &outputMaxZ) {
+	bool sameX = line.p1.x == line.p2.x;
+	bool sameY = line.p1.y == line.p2.y;
+	if (sameX && sameY) {
+		if (line.p1.x == point.x && line.p1.y == point.y) {
+			outputMinZ = min(line.p1.z, line.p2.z);
+			outputMaxZ = max(line.p1.z, line.p2.z);
+			return true;
+		}
+		return false;
+	}
+	else if (sameX || sameY) {
+		vec2 solution;
+		if (sameX) {
+			if (line.p1.x != point.x) return false;
+			mat2 system = transpose(mat2{
+				1,         1,
+				line.p1.y, line.p2.y,
+			});
+			solution = inverse(system) * vec2(1, point.y);
+		}
+		else {
+			if (line.p1.y != point.y) return false;
+			mat2 system = transpose(mat2{
+				1,         1,
+				line.p1.x, line.p2.x,
+			});
+			solution = inverse(system) * vec2(1, point.x);
+		}
+		if (solution[0] < 0 || solution[1] < 0) return false;
+		float z = (solution[0] * line.p1 + solution[1] * line.p2).z;
+		outputMinZ = z;
+		outputMaxZ = z;
+		return true;
+	}
+	else {
+		mat2 system = transpose(mat2{
+			1,         1,
+			line.p1.x, line.p2.x,
+		});
+		vec2 solution = inverse(system) * vec2(1, point.x);
+		if (solution[0] < 0 || solution[1] < 0) return false;
+		vec3 lineAtPoint = solution[0] * line.p1 + solution[1] * line.p2;
+		if (lineAtPoint.x != point.x) return false;
+		outputMinZ = lineAtPoint.z;
+		outputMaxZ = lineAtPoint.z;
+		return true;
+	}
+}
+
+// Given a point in 2d and a face, finds the minimum/maximum Z value of the face at that (x, y)
+// point. Returns false if the face has no point with that (x, y) value, and true otherwise.
+// Sets outputMinZ to the minimum face value and sets outputMaxZ with the maximum face value.
+bool GetFaceMinMaxAtPoint(const Face &face, vec2 point, float &outputMinZ, float &outputMaxZ) {
 	// We know that any point in the face can be written as a*v1 + b*v2 + c*v3, with a + b + c = 1.
 	// This gives us a set of linear equations we can solve.
-	mat3 system = transpose(mat3{1, 1, 1,
-		v1.x, v2.x, v3.x,
-		v1.y, v2.y, v3.y});
-	if (determinant(system) == 0) return false;
-	mat3 solution = inverse(system);
-	vec3 proportions = solution * vec3(1, x, y);
-	if (proportions.x < 0 || proportions.y < 0 || proportions.z < 0) return false;
-	outputZ = (proportions[0] * v1 + proportions[1] * v2 + proportions[2] * v3).z;
-	return true;
+	mat3 system = transpose(mat3{1,         1,         1,
+								 face.p1.x, face.p2.x, face.p3.x,
+								 face.p1.y, face.p2.y, face.p3.y}
+	);
+	if (determinant(system) != 0) {
+		// One unique solution exists.
+		vec3 solution = inverse(system) * vec3(1, point.x, point.y);
+		if (solution.x < 0 || solution.y < 0 || solution.z < 0) return false; // Make sure it's in the convex plane
+		float z = (solution[0] * face.p1 + solution[1] * face.p2 + solution[2] * face.p3).z;
+		outputMinZ = z;
+		outputMaxZ = z;
+		return true;
+	}
+	else {
+		// The face may appear as a line from this perspective, so let's check for all combinations
+		// of 2 points.
+		bool success = false;
+		float minZ;
+		float maxZ;
+		Line3D line;
+		line.p1 = face.p1;
+		line.p2 = face.p2;
+		if (GetLineMinMaxAtPoint(line, point, minZ, maxZ)) {
+			if (!success || minZ < outputMinZ) outputMinZ = minZ;
+			if (!success || maxZ > outputMaxZ) outputMaxZ = maxZ;
+			success = true;
+		}
+		line.p1 = face.p3;
+
+		if (GetLineMinMaxAtPoint(line, point, minZ, maxZ)) {
+			if (!success || minZ < outputMinZ) outputMinZ = minZ;
+			if (!success || maxZ > outputMaxZ) outputMaxZ = maxZ;
+			success = true;
+		}
+		line.p2 = face.p1;
+		if (GetLineMinMaxAtPoint(line, point, minZ, maxZ)) {
+			if (!success || minZ < outputMinZ) outputMinZ = minZ;
+			if (!success || maxZ > outputMaxZ) outputMaxZ = maxZ;
+			success = true;
+		}		
+		if (!success) return false;
+		return true;
+	}
 }
 
 // Finds the intersection of the lines (x1, x2) and (y1, y2) if there is one,
 // then acts like GetValueAtPoint. Returns false if there is no intersection.
-bool GetValueAtIntersection(const vec3 &v1, const vec3 &v2, const vec3 &v3,
-							const vec2 &x1, const vec2 &x2, const vec2 &y1, const vec2 &y2,
-							float &outputZ) {
-	mat2 system = {x2.x - x1.x, y1.x - y2.x,
-				   x2.y - x1.y, y1.y - y2.y};
-	if (determinant(system) == 0) return false;
-	vec2 solution = inverse(system) * vec2(y1.x - x1.x, y1.y - x1.y);
-
-	if (solution[0] < 0 || solution[0] > 1 || solution[1] < 0 || solution[1] > 1) return false; // No intersection
-	vec2 intersection = x1 + solution[0] * x2;
-	return GetValueAtPoint(v1, v2, v3, intersection.x, intersection.y, outputZ);
+bool GetFaceMinMaxAtIntersection(const Face &face,
+								 const Line2D &first, const Line2D &second,
+								 float &outputMinZ, float &outputMaxZ) {
+	vec2 intersection;
+	if (!GetIntersection(first, second, intersection)) return false;
+	return GetFaceMinMaxAtPoint(face, intersection, outputMinZ, outputMaxZ);
 }
 
 // Expands boxToExpand so the portion of the face that is within bounds all fits in boxToExpand.
 // Returns true if there is any bit of face in bounds, and false otherwise.
-bool ExpandBoxToFace(const Face &face, const Box &bounds, Box &boxToExpand) {
-	bool inBox[3];
+bool ExpandBoxAroundFace(const Face &face, const Box &bounds, Box &boxToExpand) {
+	bool inBox[3] = {false, false, false};
 	for (int axis = 0; axis < 3; axis++) {
 		// Swap axi so we don't have to worry about changing our x/y/z per direction we're looking at.
-		vec3 swappedv1 = SwapAxi(face.p1, axis);
-		vec3 swappedv2 = SwapAxi(face.p2, axis);
-		vec3 swappedv3 = SwapAxi(face.p3, axis);
+		Face swappedFace;
+		swappedFace.p1 = SwapAxi(face.p1, axis);
+		swappedFace.p2 = SwapAxi(face.p2, axis);
+		swappedFace.p3 = SwapAxi(face.p3, axis);
 
 		vec3 boxMin = SwapAxi(bounds.min, axis);
 		vec3 boxMax = SwapAxi(bounds.max, axis);
@@ -133,57 +243,49 @@ bool ExpandBoxToFace(const Face &face, const Box &bounds, Box &boxToExpand) {
 		// A corner of the (x, y) square we're looking in.
 		// An intersection of the face edge with the square edge.
 
-		float outputZ;
-		inBox[axis] = false;
+		float outputMinZ;
+		float outputMaxZ;
+
 		// Face corners.
-		for (vec3 faceCorner : {swappedv1, swappedv2, swappedv3}) {
+		for (vec3 faceCorner : {swappedFace.p1, swappedFace.p2, swappedFace.p3}) {
 			if (faceCorner.x > boxMax.x || faceCorner.x < boxMin.x ||
 				faceCorner.y > boxMax.y || faceCorner.y < boxMin.y) continue;
+			outputMinZ = faceCorner.z;
+			outputMaxZ = faceCorner.z;
 			inBox[axis] = true;
-			outputZ = faceCorner.z;
-			if (outputZ < boxToExpand.min[axis]) boxToExpand.min[axis] = outputZ;
-			if (outputZ > boxToExpand.max[axis]) boxToExpand.max[axis] = outputZ;
+			if (outputMinZ < boxToExpand.min[axis]) boxToExpand.min[axis] = outputMinZ;
+			if (outputMaxZ > boxToExpand.max[axis]) boxToExpand.max[axis] = outputMaxZ;
 		}
 
 		// Box corners.
 		for (float x : {boxMin.x, boxMax.x}) {
 			for (float y : {boxMin.y, boxMax.y}) {
-				if (GetValueAtPoint(swappedv1, swappedv2, swappedv3, x, y, outputZ)) {
+				if (GetFaceMinMaxAtPoint(swappedFace, vec2(x, y), outputMinZ, outputMaxZ)) {
 					inBox[axis] = true;
-					if (outputZ < boxToExpand.min[axis]) boxToExpand.min[axis] = outputZ;
-					if (outputZ > boxToExpand.max[axis]) boxToExpand.max[axis] = outputZ;
+					if (outputMinZ < boxToExpand.min[axis]) boxToExpand.min[axis] = outputMinZ;
+					if (outputMaxZ > boxToExpand.max[axis]) boxToExpand.max[axis] = outputMaxZ;
 				}
 			}
 		}
 
 		// Intersections
-		for (vec3 corner1 : {swappedv1, swappedv2, swappedv3}) {
-			for (vec3 corner2 : {swappedv1, swappedv2, swappedv3}) {
-				if (vec2(corner1) == vec2(corner2)) continue;
-				// Seperate code for each of the 4 box-edges since those are to ugly to iterate on.
-				if (GetValueAtIntersection(swappedv1, swappedv2, swappedv3, vec2(corner1), vec2(corner2),
-										   vec2(boxMin), vec2(boxMin.x, boxMax.y), outputZ)) {
-					inBox[axis] = true;
-					if (outputZ < boxToExpand.min[axis]) boxToExpand.min[axis] = outputZ;
-					if (outputZ > boxToExpand.max[axis]) boxToExpand.max[axis] = outputZ;
-				}
-				if (GetValueAtIntersection(swappedv1, swappedv2, swappedv3, vec2(corner1), vec2(corner2),
-										   vec2(boxMin), vec2(boxMax.x, boxMin.y), outputZ)) {
-					inBox[axis] = true;
-					if (outputZ < boxToExpand.min[axis]) boxToExpand.min[axis] = outputZ;
-					if (outputZ > boxToExpand.max[axis]) boxToExpand.max[axis] = outputZ;
-				}
-				if (GetValueAtIntersection(swappedv1, swappedv2, swappedv3, vec2(corner1), vec2(corner2),
-										   vec2(boxMax), vec2(boxMin.x, boxMax.y), outputZ)) {
-					inBox[axis] = true;
-					if (outputZ < boxToExpand.min[axis]) boxToExpand.min[axis] = outputZ;
-					if (outputZ > boxToExpand.max[axis]) boxToExpand.max[axis] = outputZ;
-				}
-				if (GetValueAtIntersection(swappedv1, swappedv2, swappedv3, vec2(corner1), vec2(corner2),
-										   vec2(boxMax), vec2(boxMax.x, boxMin.y), outputZ)) {
-					inBox[axis] = true;
-					if (outputZ < boxToExpand.min[axis]) boxToExpand.min[axis] = outputZ;
-					if (outputZ > boxToExpand.max[axis]) boxToExpand.max[axis] = outputZ;
+		Line2D first, second;
+		for (vec3 corner1 : {swappedFace.p1, swappedFace.p2, swappedFace.p3}) {
+			for (vec3 corner2 : {swappedFace.p1, swappedFace.p2, swappedFace.p3}) {
+				if (corner1 == corner2) continue;
+				first.p1 = vec2(corner1);
+				first.p2 = vec2(corner2);
+
+				for (vec2 secondStart : {vec2(boxMin), vec2(boxMax)}) {
+					for (vec2 secondEnd : {vec2(boxMin.x, boxMax.y), vec2(boxMax.x, boxMin.y)}) {
+						second.p1 = secondStart;
+						second.p2 = secondEnd;
+						if (GetFaceMinMaxAtIntersection(swappedFace, first, second, outputMinZ, outputMaxZ)) {
+							inBox[axis] = true;
+							if (outputMinZ < boxToExpand.min[axis]) boxToExpand.min[axis] = outputMinZ;
+							if (outputMaxZ > boxToExpand.max[axis]) boxToExpand.max[axis] = outputMaxZ;
+					    }
+					}
 				}
 			}
 		}
@@ -212,8 +314,6 @@ bool CollisionDetector::IsCollidingQ(std::shared_ptr<Object> toTest, Collision &
 
 bool CollisionDetector::CollidingQ(shared_ptr<Object> first, shared_ptr<Object> second, Collision &outputCollission) {
 	Box box;
-	box.min = vec3(-10, -10, -10); // TODO setup proper min/maxing for object size.
-	box.max = vec3(10, 10, 10);
 
 	shared_ptr<const Model> firstModel = first->GetModel();
 	shared_ptr<const Model> secondModel = second->GetModel();
@@ -223,8 +323,12 @@ bool CollisionDetector::CollidingQ(shared_ptr<Object> first, shared_ptr<Object> 
 	size_t secondModelNumberOfFaces = secondModel->elements.size() / 3;
 	vector<bool> firstFacesInBounds = vector<bool>(firstModelNumberOfFaces, true);
 	vector<bool> secondFacesInBounds = vector<bool>(secondModelNumberOfFaces, true);
+	float radius = max(firstModel->maxRadius, secondModel->maxRadius);
 
-	while (box.min.x < box.max.x && box.min.y < box.max.y && box.min.z < box.max.z) {
+	box.min = vec3(-radius);
+	box.max = vec3(radius);
+
+	while (box.min.x <= box.max.x && box.min.y <= box.max.y && box.min.z <= box.max.z) {
 		// Create 2 boxes wrapping around first and second object, looking only in box.
 		Box firstBox;
 		firstBox.min = box.max;
@@ -232,7 +336,7 @@ bool CollisionDetector::CollidingQ(shared_ptr<Object> first, shared_ptr<Object> 
 		for (int i = 0; i < firstModelNumberOfFaces; i++) {
 			if (!firstFacesInBounds[i]) continue;
 			Face face = GetFacePositionVerticesFromModel(firstModel, i, firstTransform);
-			firstFacesInBounds[i] = ExpandBoxToFace(face, box, firstBox);
+			firstFacesInBounds[i] = ExpandBoxAroundFace(face, box, firstBox);
 		}
 
 		Box secondBox;
@@ -241,9 +345,8 @@ bool CollisionDetector::CollidingQ(shared_ptr<Object> first, shared_ptr<Object> 
 		for (int i = 0; i < secondModelNumberOfFaces ; i++) {
 			if (!secondFacesInBounds[i]) continue;
 			Face face = GetFacePositionVerticesFromModel(secondModel, i, secondTransform);
-			secondFacesInBounds[i] = ExpandBoxToFace(face, box, secondBox);
+			secondFacesInBounds[i] = ExpandBoxAroundFace(face, box, secondBox);
 		}
-
 
 		// Get intersection of the 2 boxes.
 		Box newBox;
