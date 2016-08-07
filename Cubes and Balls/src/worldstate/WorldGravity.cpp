e9aea4b466188bad49b65c22219ed0fd2ac1b88b
Rubisk <woheri@gmail.com>
2016-08-07 13:32:02 +0200

    Made worldGravity use proper coordinate space.

--- Cubes and Balls/src/worldstate/WorldGravity.cpp
+++ Cubes and Balls/src/worldstate/WorldGravity.cpp
@@ -11,6 +11,6 @@ WorldGravity::WorldGravity(shared_ptr<WorldState> world, const vec3 &gravity) {
 void WorldGravity::GenerateForces(shared_ptr<ForceApplier> forceApplier, float timePassed) {
 	for (shared_ptr<Entity> e : world_->GetEntities()) {
 		if (dynamic_pointer_cast<Player>(e)) continue;
-		forceApplier->AddForce(e, e->GetWeight() * gravity_, vec3(0, 0, 0), timePassed);
+		forceApplier->AddForce(e, e->GetWeight() * gravity_, e->ToWorldSpace(vec3(0, 0, 0), 1), timePassed);
 	}
 }
